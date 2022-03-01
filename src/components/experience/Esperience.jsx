8a0690c7f3a58634a305feefab67a31fb891af42
cberto <bertolanicami@gmail.com>
2022-03-01 16:16:38 -0300

    first commit

---
+++ src/components/experience/Esperience.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './experience.css'
+const Esperience = () => {
+  return (
+    <div>Esperience</div>
+  )
+}
+
+export default Esperience
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './header.css'
+function Header() {
+  return (
+    <header>
+      <div className="container.header__container">
+        <h5>Hello Im </h5>
+        <h1> Camila Bertolani</h1>
+        <h5></h5>
+      </div>
+    </header>
+  )
+}
+
+export default Header
